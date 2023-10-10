aaec9ffb32bb1081e2f3a1d77fc7c96e77e7cf66
zhoracementov <zoracementov92551@gmail.com>
2023-10-10 20:32:27 +0300

    Добавьте файлы проекта.

---
+++ WPF/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static bool IsDesignMode { get; set; } = true;
+
+        private static IHost host;
+        public static IHost Host => host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            IsDesignMode = false;
+            base.OnStartup(e);
+
+            await Host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+        }
+
+        public static string DataDirectory =>
+                Path.Combine(CurrentDirectory, ConfigurationManager.AppSettings["userdata"]);
+
+        public static string CurrentDirectory => IsDesignMode
+                ? Path.GetDirectoryName(GetSourceCodePath())
+                : Environment.CurrentDirectory;
+
+        public static string GetSourceCodePath([CallerFilePath] string path = null) => path;
+    }
+}
---
+++ WPF/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Configuration;
+using WPF.ViewModels;
+
+namespace WPF
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] agrs) => Host
+                .CreateDefaultBuilder(agrs)
+                .UseContentRoot(App.DataDirectory);
+        //        .ConfigureAppConfiguration(ConfigureAppConfiguration)
+        //        .ConfigureServices(ConfigurateServices);
+
+        //public static void ConfigureAppConfiguration(HostBuilderContext host, IConfigurationBuilder cfg) => cfg
+        //        .SetBasePath(App.DataDirectory);
+        //.AddJsonFile(App.SettingsFileName, optional: false, reloadOnChange: true);
+
+        //public static void ConfigurateServices(HostBuilderContext host, IServiceCollection services) => services
+        //        .AddSingleton<Func<Type, ViewModel>>(sp => vmt => (ViewModel)sp.GetRequiredService(vmt));
+        //.ConfigureWritable<GameSettings>(host.Configuration.GetSection(nameof(GameSettings)), App.SettingsFileName);
+    }
+}
